5fc22f2d57d7e1cec8c64b2b407e2397e876eec4
Kohei Kakimoto <naige.1028@gmail.com>
2016-11-05 22:19:16 +0900

    Create user registration page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get '/register', to: 'register#index'
+
   root to: 'top#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
